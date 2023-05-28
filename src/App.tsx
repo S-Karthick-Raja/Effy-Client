e0cd0ade01c7a24190ee8125b5ebd8f261285643
S-Karthick-Raja <kraja2796@gmail.com>
2023-05-28 17:11:43 +0530

    feat: configuration done for react router dom and jotai provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import HomeLayout from "./layouts/homeLayout";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home";
+import DashboardPage from "./pages/dashboard";
+import CompaniesPage from "./pages/companies";
+import UsersPage from "./pages/users";
+import DeactivatedUserPage from "./pages/deactivated";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="">
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomeLayout />}>
+            <Route path="" element={<HomePage />} />
+            <Route path="dashboard" element={<DashboardPage />} />
+            <Route path="companies" element={<CompaniesPage />} />
+            <Route path="users" element={<UsersPage />} />
+            <Route path="deactivated/users" element={<DeactivatedUserPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
