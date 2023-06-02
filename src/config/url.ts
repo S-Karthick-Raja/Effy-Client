6d98e4231257bc91d75f9fa295d44a6d1fabb0c0
S-Karthick-Raja <kraja2796@gmail.com>
2023-06-02 12:58:58 +0530

    feat: added the urls

--- src/config/url.ts
+++ src/config/url.ts
@@ -4,4 +4,32 @@ export const getUniqueCompany = '/company/get/unique'
 
 export const addCompany = '/company/create/new/company'
 
+export const updateCompany = '/company/update/unique'
+
 export const deleteCompany = '/company/delete/unique'
+
+export const getAllUsers = '/users/get/all/usersData'
+
+export const getUniqueUser = '/users/get/unique'
+
+export const addUser = '/users/create/new/user'
+
+export const deleteUser = '/users/delete/unique'
+
+export const assignUserToCompany = '/users/assign/company'
+
+export const migrateUser = '/users/migrate'
+
+export const deactivateUser = '/users/deactivate/unique'
+
+export const getAllUserFromCompany = '/company/get/all/users'
+
+export const removeUserFromCompany = '/users/remove/user/company'
+
+export const fetchCompanyList = '/users/get/all/company/list'
+
+export const fetchDesignationList = '/users/get/all/designation/list'
+
+export const getAllDeactivatedUsers = '/users/get/all/deactivated/users'
+
+export const updateUser = '/users/update/unique'
