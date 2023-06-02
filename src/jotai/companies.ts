75ecf85d5b9075995e58a19ed64defbf00962648
S-Karthick-Raja <kraja2796@gmail.com>
2023-06-02 13:00:36 +0530

    feat: added the jotai state for the app

--- src/jotai/companies.ts
+++ src/jotai/companies.ts
@@ -3,3 +3,5 @@ import { atom } from "jotai";
 export const LoadingState = atom(false);
 export const InitialDelPopupCompany = atom(false)
 export const InitialEditPopupCompany = atom(false)
+
+export const InitialSelectedCompany = atom([])
---
+++ src/jotai/global.ts
@@ -0,0 +1,6 @@
+import { atom } from "jotai";
+
+export const InitialAddCompanyPopup = atom(false)
+export const InitialAddUserPopup = atom(false)
+
+export const InitialAddUserToSpeCompany = atom(false)
