cf170a756dec4e41d3125d8f2ef5c4718faab1bc
S-Karthick-Raja <kraja2796@gmail.com>
2023-06-02 12:57:34 +0530

    refactor: changed some route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import HomeLayout from "./layouts/homeLayout";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/home";
 import DashboardPage from "./pages/dashboard";
 import CompaniesPage from "./pages/companies";
 import UsersPage from "./pages/users";
@@ -9,6 +8,7 @@ import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import UniqueCompany from "./pages/uniqueCompany";
 import CompanyAddress from "./pages/companyAddress";
+import UniqueUser from "./pages/uniqueUser";
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -18,12 +18,13 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<HomeLayout />}>
-            <Route path="" element={<HomePage />} />
-            <Route path="dashboard" element={<DashboardPage />} />
+            <Route path="" element={<DashboardPage />} />
             <Route path="companies" element={<CompaniesPage />} />
+            <Route path="companies/update/:id" element={<CompaniesPage />} />
             <Route path="company/:id" element={<UniqueCompany />} />
             <Route path="company/address/:id" element={<CompanyAddress />} />
             <Route path="users" element={<UsersPage />} />
+            <Route path="user/:id" element={<UniqueUser />} />
             <Route path="deactivated/users" element={<DeactivatedUserPage />} />
           </Route>
         </Routes>
